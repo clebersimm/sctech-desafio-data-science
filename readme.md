3a009aac09518a89bc98c162b8e140edac2ef39e
Cleber Simm <clebersimmdev@gmail.com>
2026-03-08 20:49:07 -0300

    Refactor code structure for improved readability and maintainability

--- readme.md
+++ readme.md
@@ -18,24 +18,25 @@ comparações entre grupos e análises exploratórias das variáveis disponívei
 
 |Coluna|Descrição|Tipo|
 |---|---|---|
-|PassengerId||Numérico|
-|Survived||Numerico 0/1|
-|Pclass||Numerico|
+|PassengerId|Identificação do passageiro|Numérico|
+|Survived|1 - sobreviveu, 0 - não sobreviveu|Numerico 0/1|
+|Pclass|1- primeira(convés superior), 2- segunda(meio), 3- terceira(convés inferior)|Numerico|
 |Name|Nome dos passageiros|texto|
-|Sex||text male/female|
-|Age||numerico|
-|SibSp||numerico|
-|Parch||numerico|
-|Ticket||texto|
-|Fare||dinheiro|
-|Cabin||texto|
-|Embarked||texto|
+|Sex|genero|text male/female|
+|Age|Idade|numerico|
+|SibSp|Irmãos/conjuges|numerico|
+|Parch|pais/filhos|numerico|
+|Ticket|bilhete|texto|
+|Fare|tarifa|dinheiro|
+|Cabin|cabine|texto|
+|Embarked|porto de embarque|texto|
 
 ## Ferramentas utilizadas
 
 - vscode
   - Extensões: github copilot, markdown preview, pylance, python, python debugger, python environments
 - python: python3.12.3
+  - libs: pandas, matplotlib
 
 ## Desenvolvimento do projeto  
 
@@ -44,7 +45,15 @@ comparações entre grupos e análises exploratórias das variáveis disponívei
 - Todas as atividades são executas na pasta sctech-desafio-data-science
 - Criar nova pasta para armazenamento de dados, desta forma mantendo separação dos arquivos e fonte de dados(arquivos).
 - Instalar o pandas: uv add pandas
+- Instalar matplot: uv add matplotlib
 
 ## Executar o projeto   
 
 no terminal, na pasta sctech-desafio-data-science, executar uv run main.py
+
+
+## Relatórios  
+
+- sobreviventes por genero: Total de sobreviventes por genero em relação ao total de sobreviventes
+- sobreviventes por genero e classe social
+- sobreviventes por idade
--- sctech-desafio-data-science/main.py
+++ sctech-desafio-data-science/main.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import os
 
 def carregar_arquivo_csv(caminho_arquivo):
     try:
         df = pd.read_csv(caminho_arquivo)
-        print(f"Arquivo '{caminho_arquivo}' carregado com sucesso!")
+        #print(f"Arquivo '{caminho_arquivo}' carregado com sucesso!")
         return df
     except FileNotFoundError:
         print(f"Erro: O arquivo '{caminho_arquivo}' não foi encontrado.")
@@ -16,16 +18,20 @@ def carregar_arquivo_csv(caminho_arquivo):
         print(f"Ocorreu um erro ao carregar o arquivo: {e}")
 
 def remover_linhas_duplicadas(df):
-    print(f"Verificando linhas duplicadas. Total de linhas antes da limpeza: {len(df)}")
+    #print(f"Verificando linhas duplicadas. Total de linhas antes da limpeza: {len(df)}")
     total_linhas_duplicadas = df.duplicated().sum()
-    print(f"Total de linhas duplicadas encontradas: {total_linhas_duplicadas}")
+    #print(f"Total de linhas duplicadas encontradas: {total_linhas_duplicadas}")
     if total_linhas_duplicadas > 0:
         df.drop_duplicates(keep='last', inplace=True)
-        print(f"Linhas duplicadas removidas. Total de linhas após limpeza: {len(df)}")
+     #   print(f"Linhas duplicadas removidas. Total de linhas após limpeza: {len(df)}")
     return df
 
+def remover_colunas_desnecessarias(df, colunas_para_remover):
+    #print(f"Removendo colunas desnecessárias: {colunas_para_remover}")
+    df.drop(columns=colunas_para_remover, inplace=True)
+
 def ajustar_nomeclatura_colunas(df):
-    print("Ajustando a nomenclatura das colunas para português...")
+    #print("Ajustando a nomenclatura das colunas para português...")
     df.rename(columns={
         'PassengerId':'identificacao_passageiro',
         'Survived':'sobreviveu',
@@ -38,7 +44,7 @@ def ajustar_nomeclatura_colunas(df):
         'Ticket':'bilhete',
         'Fare':'tarifa',
         'Cabin':'cabine',
-        'Embarked':'embarque'
+        'Embarked':'porto_de_embarque'
         }, inplace=True)
 
 def ajustar_valores_nulos(df):
@@ -51,21 +57,89 @@ def relacao_sobrevivencia_por_colunas(df, coluna):
         'total_passageiros': total_passageiros_por_coluna,
         'sobreviventes': sobreviventes_por_coluna
     })
-    contagem['relacao_sobrevivencia'] = contagem['sobreviventes'] / contagem['total_passageiros']
+    contagem['relacao_sobrevivencia'] = (contagem['sobreviventes'] / contagem['total_passageiros']) * 100
+    print(contagem)
     return contagem
+
+def gravar_arquivo(plt, nome_arquivo):
+    diretorio_saida = "relatorios"
+    if not os.path.exists(diretorio_saida):
+        os.makedirs(diretorio_saida)
+    caminho_completo = os.path.join(diretorio_saida, nome_arquivo)
+    plt.savefig(caminho_completo)
+
+
+def plotar_grafico_pizza(dados_processados, dados_plotar_grafico, gerar_arquivo=False):
+    contagem = dados_processados[dados_plotar_grafico['indice']][dados_plotar_grafico['coluna']]
+    plt.figure(figsize=(8, 8))
+    plt.pie(contagem, labels=contagem.index, autopct='%1.1f%%', startangle=140)
+    plt.title(dados_plotar_grafico['titulo'])
+    plt.axis('equal')
+    plt.legend(title=dados_plotar_grafico['coluna'], loc='lower right')
+    if gerar_arquivo:
+        gravar_arquivo(plt, dados_plotar_grafico['arquivo_saida'])
+    else:
+        plt.show()
+"""
+def plotar_grafico_barra(dados_processados, dados_plotar_grafico, gerar_arquivo=False):
+    contagem = dados_processados[dados_plotar_grafico['indice']][dados_plotar_grafico['coluna']]
+    plt.figure(figsize=(10, 6))
+    contagem.plot(kind='bar')
+    plt.title(dados_plotar_grafico['titulo'])
+    plt.xlabel(dados_plotar_grafico['legenda_eixo_x'])
+    plt.ylabel('Total')
+    plt.xticks(rotation=45)
+    if gerar_arquivo:
+        gravar_arquivo(plt, dados_plotar_grafico['arquivo_saida'])
+    else:
+        plt.show()
+"""
+
+def plotar_grafico_barra(dados_processados, dados_plotar_grafico, gerar_arquivo=False):
+    plt.plot(dados_processados)
+    plt.figure(figsize=(10, 6))
+    dados_processados.plot(kind='bar')
+    plt.title(dados_plotar_grafico['titulo'])
+    plt.xlabel(dados_plotar_grafico['legenda_eixo_x'])
+    plt.ylabel('Total')
+    plt.xticks(rotation=45)
+    if gerar_arquivo:
+        gravar_arquivo(plt, dados_plotar_grafico['arquivo_saida'])
+    else:
+        plt.show()
 
 def main():
     df = carregar_arquivo_csv("data/titanic_dataset.csv")
+    remover_colunas_desnecessarias(df, ['Name', 'Ticket', 'Fare', 'Cabin', 'Embarked'])
     remover_linhas_duplicadas(df)
-    ajustar_nomeclatura_colunas(df)
     ajustar_valores_nulos(df)
+    ajustar_nomeclatura_colunas(df)
+    total_passageiros = len(df)
+    total_sobreviventes = df.query("sobreviveu == 1")['sobreviveu'].count()
+    print(df.groupby('genero').size())
     dados_processados = {
+        'total_passageiros': total_passageiros,
+        'total_sobreviventes': total_sobreviventes,
         'relacao_sobrevivencia_por_classe': relacao_sobrevivencia_por_colunas(df, ['classe']),
         'relacao_sobrevivencia_por_genero': relacao_sobrevivencia_por_colunas(df, ['genero']),
         'relacao_sobrevivencia_por_genero_classe': relacao_sobrevivencia_por_colunas(df, ['genero', 'classe'])
     }
-    print(dados_processados)
-
+    
+    #print(dados_processados)
+    plotar_grafico_barra(dados_processados['relacao_sobrevivencia_por_genero'], {
+        'indice': 'relacao_sobrevivencia_por_genero',
+        'legenda_eixo_x': 'Gênero',
+        'titulo': 'Relação de sobreviventes por gênero',
+        'arquivo_saida': 'relacao_sobreviventes_por_genero.png'
+    },gerar_arquivo=False)
+    """
+    plotar_grafico_pizza(dados_processados, {
+        'indice': 'relacao_sobrevivencia_por_genero_classe',
+        'coluna': 'sobreviventes',
+        'titulo': 'Relação de sobreviventes por gênero e classe',
+        'arquivo_saida': 'relacao_sobreviventes_por_genero_e_classe.png'
+    },gerar_arquivo=False)
+    """
 
 if __name__ == "__main__":
     main()
